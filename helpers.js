8e5a189a4eeea6aae35a7a24cfda6dbb98cb79bd
Miguel Jimenez <migang9@gmail.com>
2021-07-10 18:32:38 -0400

    moved in the generateRandomString function

--- helpers.js
+++ helpers.js
@@ -8,4 +8,10 @@ const findUserByEmail = (email, users) => {
   return false;
 };
 
-module.exports = { findUserByEmail };
+// Function to generate random string of six alphanumeric value
+const generateRandomString = () => {
+  const random = Math.random().toString(36).substring(2,8);
+  return random;
+};
+
+module.exports = { findUserByEmail, generateRandomString };
